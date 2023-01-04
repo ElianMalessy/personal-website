c38405a5142944de42b83cbb4c50a9c990447b32
Elian Malessy <eql458@gmail.com>
2023-01-04 15:11:08 -0800

    fixes

--- pages/api/index.js
+++
@@ -1,8 +0,0 @@
-import { setCookie } from "cookies-next";
-
-export default async function handler(req, res) {
-  const { color } = req.query;
-  setCookie("chakra-ui-color-mode", color, { req, res });
-  console.log(color);
-  res.end(JSON.stringify(color));
-}
