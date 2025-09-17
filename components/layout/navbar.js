ac6fd7f9e66cff5826774eeaee2a3ce465a709f7
Elian Hijmans Malessy <ehijmans@uci.edu>
2025-09-17 01:04:28 -0700

    changed resume to google drive

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -147,7 +147,7 @@ const Navbar = () => {
             LinkedIn
           </LinkItem>
           <LinkItem
-            href="https://firebasestorage.googleapis.com/v0/b/personal-w-51f5c.appspot.com/o/Elian_Hijmans_Malessy_resume.pdf?alt=media&token=967f36d4-1777-4c79-8efc-336d3381823c"
+            href="https://drive.google.com/file/d/1xyuZxmHP0vLTS4WxGN467-vS092jVywt/view?usp=sharing"
             display="inline-flex"
             alignItems="center"
             style={{ gap: 4, marginRight: "1rem" }}
@@ -186,7 +186,7 @@ const Navbar = () => {
                   <MenuItem as={Link}>Github</MenuItem>
                 </NextLink>
                 <NextLink
-                  href="https://firebasestorage.googleapis.com/v0/b/personal-w-51f5c.appspot.com/o/Elian_Hijmans_Malessy_resume.pdf?alt=media&token=967f36d4-1777-4c79-8efc-336d3381823c"
+                  href="https://drive.google.com/file/d/1xyuZxmHP0vLTS4WxGN467-vS092jVywt/view?usp=sharing"
                   passHref
                 >
                   <MenuItem as={Link}>Resume</MenuItem>
